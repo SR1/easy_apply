13327fcdc5c3f6b522182db09ee80b90bfffc377
SR1 <sr1@SR1-VirtualBox.(none)>
2013-11-21 03:10:53 +0800

    add detail page

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/detail")
+def detail():
+    return render_template('detail.html')
+
 if __name__=="__main__":
     app.run(debug=True)
