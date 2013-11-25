31780b7bc401e2292f569d6aab3e5bdb8b23d544
SR1 <seems.never.mind@gmail.com>
2013-11-26 07:53:16 +0800

    allow access from all

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
+app.debug=True
+app.host="0.0.0.0"
 
 @app.route("/")
 def index():
@@ -18,4 +20,4 @@ def test(username):
     return url_for("apply")
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
